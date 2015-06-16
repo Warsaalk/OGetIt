3578e28d6853e3f0cd6026af58c6afb7bc25d7a3
Klaas Van Parys <accounts@klaas.cc>
2015-06-16 16:02:30 +0200

    Resources Class

---
+++ ogetit/common/ogetit_resources.php
@@ -0,0 +1,61 @@
+<?php
+namespace OGetIt\Common;
+
+class Resources {
+	
+	/**
+	 * @var integer
+	 */
+	private $_metal;
+
+	/**
+	 * @var integer
+	 */
+	private $_crystal;
+
+	/**
+	 * @var integer
+	 */
+	private $_deuterium;
+	
+	/**
+	 * @param integer $metal
+	 * @param integer $crystal
+	 * @param integer $deuterium
+	 */
+	public function __construct($metal, $crystal, $deuterium) {
+		
+		$this->_metal = $metal;
+		$this->_crystal = $crystal;
+		$this->_deuterium = $deuterium;
+		
+	}
+	
+	/**
+	 * @return integer
+	 */
+	public function getMetal() {
+		
+		return $this->_metal;
+		
+	}
+
+	/**
+	 * @return integer
+	 */
+	public function getCrystal() {
+		
+		return $this->_crystal;
+		
+	}
+
+	/**
+	 * @return integer
+	 */
+	public function getDeuterium() {
+		
+		return $this->_deuterium;
+		
+	}
+	
+}
