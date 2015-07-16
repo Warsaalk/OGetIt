8269ec04391a3fe93b92b2392009c4f56a35639e
Klaas Van Parys <accounts@klaas.cc>
2015-07-16 17:54:33 +0200

    Use Coordinates class

--- ogetit/common/ogetit_planet.php
+++ ogetit/common/ogetit_planet.php
@@ -19,7 +19,7 @@
  */
 namespace OGetIt\Common;
 
-class OGetIt_Planet {
+class OGetIt_Planet extends OGetIt_Coordinates {
 
 	const	TYPE_PLANET = 1,
 			TYPE_MOON = 3;
@@ -32,59 +32,17 @@ class OGetIt_Planet {
 	/**
 	 * @var string
 	 */
-	private $_coordinates;
-	
-	/**
-	 * @var string
-	 */
 	private $_name;
-	
-	/**
-	 * @var integer
-	 */
-	private $_galaxy;
-
-	/**
-	 * @var integer
-	 */
-	private $_system;
-
-	/**
-	 * @var integer
-	 */
-	private $_position;
-	
 	/**
 	 * @param string $type
 	 * @param string $coordinates
 	 */
 	public function __construct($type, $coordinates, $name = null) {
 		
+		parent::__construct($coordinates);
+		
 		$this->_type = $type;
-		$this->_coordinates = $coordinates;
 		$this->_name = $name;
-		
-		$coordinatesObject = self::parseCoordinates($coordinates);
-		
-		$this->_galaxy = $coordinatesObject->galaxy;
-		$this->_system = $coordinatesObject->system;
-		$this->_position = $coordinatesObject->position;
-		
-	}
-	
-	/**
-	 * @param string $coordinates
-	 * @return \stdClass
-	 */
-	public static function parseCoordinates($coordinates) {
-		
-		$coordinatesArray = explode(':', $coordinates);
-		$coordinatesObject = new \stdClass();
-		$coordinatesObject->galaxy = (int)$coordinatesArray[0];
-		$coordinatesObject->system = (int)$coordinatesArray[1];
-		$coordinatesObject->position = (int)$coordinatesArray[2];
-		
-		return $coordinatesObject;
 		
 	}
 	
@@ -96,15 +54,6 @@ class OGetIt_Planet {
 		return $this->_type;
 		
 	}
-
-	/**
-	 * @return string
-	 */
-	public function getCoordinates() {
-		
-		return $this->_coordinates;
-		
-	}
 	
 	/**
 	 * @return string
@@ -112,33 +61,6 @@ class OGetIt_Planet {
 	public function getName() {
 		
 		return $this->_name;
-		
-	}
-
-	/**
-	 * @return integer
-	 */
-	public function getGalaxy() {
-		
-		return $this->_galaxy;
-		
-	}
-
-	/**
-	 * @return integer
-	 */
-	public function getSystem() {
-		
-		return $this->_system;
-		
-	}
-
-	/**
-	 * @return integer
-	 */
-	public function getPosition() {
-		
-		return $this->_position;
 		
 	}
 	
