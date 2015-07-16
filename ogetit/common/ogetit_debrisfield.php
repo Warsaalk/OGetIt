b20f39c5b1402043eade047054288f40fccdd0df
Klaas Van Parys <accounts@klaas.cc>
2015-07-16 17:55:20 +0200

    Added debrisfield class

---
+++ ogetit/common/ogetit_debrisfield.php
@@ -0,0 +1,68 @@
+<?php
+/*
+ * Copyright © 2015 Klaas Van Parys
+ * 
+ * This file is part of OGetIt.
+ * 
+ * OGetIt is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * OGetIt is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with OGetIt.  If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace OGetIt\Common;
+
+class OGetIt_DebrisField extends OGetIt_Coordinates {
+
+	/**
+	 * @var OGetIt_Resources
+	 */
+	private $_resources;
+	
+	/**
+	 * @param string $type
+	 * @param string $coordinates
+	 */
+	public function __construct($coordinates, $metal, $crystal) {
+		
+		parent::__construct($coordinates);
+
+		$this->_resources = new OGetIt_Resources($metal, $crystal, 0);
+		
+	}
+	
+	/**
+	 * @return integer
+	 */
+	public function getMetal() {
+		
+		return $this->_resources->getMetal();
+		
+	}
+
+	/**
+	 * @return integer
+	 */
+	public function getCrystal() {
+		
+		return $this->_resources->getCrystal();
+		
+	}
+	
+	/**
+	 * @return OGetIt_Resources
+	 */
+	public function getResources() {
+		
+		return $this->_resources;
+		
+	}
+	
+}
