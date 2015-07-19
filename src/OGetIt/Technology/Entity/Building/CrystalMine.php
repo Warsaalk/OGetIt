1dc0817c4e8f3c1af4b4741c720dd190f87cfbaa
Klaas Van Parys <accounts@klaas.cc>
2015-07-19 12:24:47 +0200

    Added classes for buildings & research

---
+++ src/OGetIt/Technology/Entity/Building/CrystalMine.php
@@ -0,0 +1,41 @@
+<?php
+/*
+ * Copyright © 2015 Klaas Van Parys
+ * 
+ * This file is part of OGetIt.
+ * 
+ * OGetIt is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * OGetIt is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with OGetIt.  If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace OGetIt\Technology\Entity\Building; 
+
+use OGetIt\Technology\Technology_Economy;
+
+class DeuteriumSynthesizer extends Technology_Economy {
+	
+	const TYPE = 3;
+	
+	const METAL = 0, CRYSTAL = 0, DEUTERIUM = 0;
+	
+	/**
+	 * @var integer
+	 */
+	protected $power_base = 1.5;
+	
+	public function __construct() {
+		
+		parent::__construct(self::TYPE, self::METAL, self::CRYSTAL, self::DEUTERIUM);
+		
+	}
+	
+}
