d07c0147f3b94279d88e1ca76f5398513c64c64d
Klaas Van Parys <warsaalk@gmail.com>
2015-08-23 12:00:12 +0200

    Added planet trait

---
+++ src/OGetIt/Common/PlanetTrait.php
@@ -0,0 +1,46 @@
+<?php
+/*
+ * Copyright © 2015 Klaas Van Parys
+ *
+ * This file is part of OGetIt.
+ *
+ * OGetIt is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OGetIt is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with OGetIt.  If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace OGetIt\Common;
+
+use OGetIt\Common\Planet;
+
+trait PlanetTrait {
+	
+	private $_planet;
+	
+	/**
+	 * @param Planet $planet
+	 */
+	public function setPlanet(Planet $planet) {
+		
+		$this->_planet = $planet;
+		
+	}
+	
+	/**
+	 * @return Planet
+	 */
+	public function getPlanet() {
+		
+		return $this->_planet;
+		
+	}
+	
+}
